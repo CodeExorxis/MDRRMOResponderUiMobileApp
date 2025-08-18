86bc5aab499827fff86ce32d753b0f04252bb901
CodeExorxiS <kgabrez8@gmail.com>
2025-08-18 18:58:58 +0800

    root binding in activity main

--- app/src/main/java/com/example/rescueappforresponder/MainActivity.java
+++ app/src/main/java/com/example/rescueappforresponder/MainActivity.java
@@ -1,24 +1,74 @@
 package com.example.rescueappforresponder;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
+import com.example.rescueappforresponder.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
+
+    // Declare binding
+    private ActivityMainBinding root;
+    String selectedStatus = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        root = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(root.getRoot());
+
+        //showing the status report category
+        root.btnStatusReport.setOnClickListener(v -> {
+            root.statusReportCategories.setVisibility(View.VISIBLE);
+            root.btnStatusReport.setVisibility(View.GONE);
+            root.btnPcrReport.setVisibility(View.GONE);
         });
+        //Showing the PCR report category
+        root.btnPcrReport.setOnClickListener(v -> {
+            root.PCRFormCategory.setVisibility(View.VISIBLE);
+            root.btnStatusReport.setVisibility(View.GONE);
+            root.btnPcrReport.setVisibility(View.GONE);
+        });
+
+        // --- Vehicular options ---
+        root.btnOnScene.setOnClickListener(v -> {
+            selectedStatus = "On Scene";
+            resetSelectectedStatus();
+            root.btnOnScene.setBackgroundResource(R.drawable.btn_selected_flat);
+        });
+
+        root.btnTransferPatient.setOnClickListener(v -> {
+            selectedStatus = "Transferring Patient";
+            resetSelectectedStatus();
+            root.btnTransferPatient.setBackgroundResource(R.drawable.btn_selected_flat);
+        });
+
+        root.btnCompleted.setOnClickListener(v -> {
+            selectedStatus = "Completed";
+            resetSelectectedStatus();
+            root.btnCompleted.setBackgroundResource(R.drawable.btn_selected_flat);
+        });
+
+        root.btnSubmitStatusReport.setOnClickListener(v -> {
+            if (selectedStatus.isEmpty()) {
+                Toast.makeText(this, "Please select a vehicular condition.", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            Toast.makeText(this, "Submitted: " + selectedStatus, Toast.LENGTH_SHORT).show();
+            root.statusReportCategories.setVisibility(View.GONE);
+            root.btnStatusReport.setVisibility(View.VISIBLE);
+            root.btnPcrReport.setVisibility(View.VISIBLE);
+            resetSelectectedStatus();
+            selectedStatus = "";
+        });
+    }
+    private void resetSelectectedStatus() {
+        root.btnOnScene.setBackgroundResource(R.drawable.btn_flat_red);
+        root.btnTransferPatient.setBackgroundResource(R.drawable.btn_flat_red);
+        root.btnCompleted.setBackgroundResource(R.drawable.btn_flat_red);
     }
 }
